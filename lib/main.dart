6ef3dee077e486a590e4a0fe4a764d76a0c08d9c
Aung Myo Min <70466675+aungmyominoffical@users.noreply.github.com>
2023-08-30 08:02:04 -0700

    Update Package Name

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ibet789 - Myanmar',
+      title: '789Maung',
       theme: ThemeData(
         // This is the theme of your application.
         //
